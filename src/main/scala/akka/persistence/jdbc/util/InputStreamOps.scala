67a6c9843000ed3525911f66f0fb9e48886522fd
Ruud Welling <wellingr@users.noreply.github.com>
2019-01-15 19:47:02 +0100

    Use a more imperative algorithm for converting an inputstream to a bytearray, which should save a lot of memory

--- src/main/scala/akka/persistence/jdbc/util/InputStreamOps.scala
+++ src/main/scala/akka/persistence/jdbc/util/InputStreamOps.scala
@@ -16,10 +16,22 @@
 
 package akka.persistence.jdbc.util
 
-import java.io.InputStream
+import java.io.{ByteArrayOutputStream, InputStream}
+
+import scala.concurrent.blocking
 
 object InputStreamOps {
-  implicit class InputStreamImplicits(val that: InputStream) extends AnyVal {
-    def toArray: Array[Byte] = Stream.continually(that.read).takeWhile(_ != -1).map(_.toByte).toArray
+  implicit class InputStreamImplicits(val is: InputStream) extends AnyVal {
+    def toArray: Array[Byte] = blocking {
+      // based on https://stackoverflow.com/a/17861016/865265
+      val bos: ByteArrayOutputStream = new ByteArrayOutputStream
+      val buffer: Array[Byte] = new Array[Byte](0xFFFF)
+      var len: Int = is.read(buffer)
+      while (len != -1) {
+        bos.write(buffer, 0, len)
+        len = is.read(buffer)
+      }
+      bos.toByteArray
+    }
   }
 }
